2eb202e951302d651b2e168793c31147a0361a43
pdetli <74938586+pdetli@users.noreply.github.com>
2021-04-02 22:12:08 +0200

    Add files via upload
    Emojipedia Mapping Components Practice

---
+++ emojipedia.js
@@ -0,0 +1,48 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 4,
+    emoji: "🥺",
+    name: "Glossy Eyes, Laughing",
+    meaning:
+      "“Puppy dog” eyes, as if begging or pleading. May also represent adoration or feeling touched by a loving gesture."
+  },
+  {
+    id: 5,
+    emoji: "🤩",
+    name: "Star-Struck",
+    meaning:
+      "May express that someone or something is amazing, fascinating, impressive, or exciting."
+  },
+
+  {
+    id: 6,
+    emoji: "😑",
+    name: "Expressionless Face",
+    meaning:
+      "May convey a sense of frustration or annoyance more intense than suggested by Neutral Face, as if taking a moment to collect itself."
+  }
+];
+
+export default emojipedia;
